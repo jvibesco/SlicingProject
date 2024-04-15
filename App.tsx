029b4e6154b364b40051340d84805bff3e70a443
jvibesco <vibescojordan@gmail.com>
2024-04-15 21:19:06 +0700

    new project: Slicing Project

--- App.tsx
+++ App.tsx
@@ -1,118 +1,425 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import 'react-native-gesture-handler';
+import {Button, StyleSheet, Text, View, ScrollView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// const [jurusan, setJurusan] = useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const [tab, setTab] = useState('new');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const header = () => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          gap: 10,
+          paddingVertical: 20,
+          paddingHorizontal: 20,
+        }}>
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+        <TouchableOpacity
+          onPress={() => setTab('new')}
+          style={{backgroundColor: 'green', borderRadius: 15}}>
+          <Text style={{paddingHorizontal: 20, color: 'white'}}>New</Text>
+        </TouchableOpacity>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        <TouchableOpacity
+          onPress={() => setTab('confirm')}
+          style={{backgroundColor: 'blue', borderRadius: 15}}>
+          <Text style={{paddingHorizontal: 20, color: 'white'}}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
+    );
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  const home = () => {
+    return (
+      <View style={{flex: 1, paddingHorizontal: 20}}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <TouchableOpacity
+            style={{
+              shadowOpacity: 0.27,
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 10,
+              backgroundColor: 'white',
+              shadowRadius: 4.65,
+              elevation: 10,
+            }}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                SBOOKXXXXXXX
+              </Text>
+              <View
+                style={{
+                  backgroundColor: 'green',
+                  borderRadius: 10,
+                  paddingHorizontal: 12,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{color: 'white'}}>New</Text>
+              </View>
+            </View>
+            <Text style={{fontSize: 14, color: 'black'}}>16/03/2024</Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                marginVertical: 10, // Sesuaikan dengan margin yang diinginkan
+              }}
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>
+                09123213213
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                  color: 'blue',
+                }}>
+                Warranty
+              </Text>
+            </View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              NBAXZBOOK
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>
+              Mybook 8/SSD256/Core-i3/Blue
+            </Text>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              P. Amir - 081399999999
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>LCD Rusak</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              shadowOpacity: 0.27,
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 10,
+              backgroundColor: 'white',
+              shadowRadius: 4.65,
+              elevation: 10,
+              marginVertical: 15,
+            }}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                SBOOKXXXXXXX
+              </Text>
+              <View
+                style={{
+                  backgroundColor: 'green',
+                  borderRadius: 10,
+                  paddingHorizontal: 12,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{color: 'white'}}>New</Text>
+              </View>
+            </View>
+            <Text style={{fontSize: 14, color: 'black'}}>16/03/2024</Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                marginVertical: 10, // Sesuaikan dengan margin yang diinginkan
+              }}
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>
+                09123213213
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                  color: 'blue',
+                }}>
+                Warranty
+              </Text>
+            </View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              NBAXZBOOK
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>
+              Mybook 8/SSD256/Core-i3/Blue
+            </Text>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              P. Amir - 081399999999
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>LCD Rusak</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              shadowOpacity: 0.27,
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 10,
+              backgroundColor: 'white',
+              shadowRadius: 4.65,
+              elevation: 10,
+              marginVertical: 15,
+            }}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                SBOOKXXXXXXX
+              </Text>
+              <View
+                style={{
+                  backgroundColor: 'green',
+                  borderRadius: 10,
+                  paddingHorizontal: 12,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{color: 'white'}}>New</Text>
+              </View>
+            </View>
+            <Text style={{fontSize: 14, color: 'black'}}>16/03/2024</Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                marginVertical: 10, // Sesuaikan dengan margin yang diinginkan
+              }}
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>
+                09123213213
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                  color: 'blue',
+                }}>
+                Warranty
+              </Text>
+            </View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              NBAXZBOOK
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>
+              Mybook 8/SSD256/Core-i3/Blue
+            </Text>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              P. Amir - 081399999999
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>LCD Rusak</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              shadowOpacity: 0.27,
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 10,
+              backgroundColor: 'white',
+              shadowRadius: 4.65,
+              elevation: 10,
+              marginVertical: 15,
+            }}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                SBOOKXXXXXXX
+              </Text>
+              <View
+                style={{
+                  backgroundColor: 'green',
+                  borderRadius: 10,
+                  paddingHorizontal: 12,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{color: 'white'}}>New</Text>
+              </View>
+            </View>
+            <Text style={{fontSize: 14, color: 'black'}}>16/03/2024</Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                marginVertical: 10, // Sesuaikan dengan margin yang diinginkan
+              }}
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>
+                09123213213
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                  color: 'blue',
+                }}>
+                Warranty
+              </Text>
+            </View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              NBAXZBOOK
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>
+              Mybook 8/SSD256/Core-i3/Blue
+            </Text>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              P. Amir - 081399999999
+            </Text>
+            <Text style={{fontSize: 14, color: 'black'}}>LCD Rusak</Text>
+          </TouchableOpacity>
+          {/* <Text>Home Screen</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            /> */}
+        </ScrollView>
+      </View>
+    );
+  };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  const detailScreen = () => {
+    return (
+      <View style={{flex: 1, paddingHorizontal: 20}}>
+        <TouchableOpacity
+          style={{
+            shadowOpacity: 0.27,
+            paddingHorizontal: 17,
+            paddingVertical: 10,
+            borderRadius: 10,
+            backgroundColor: 'white',
+            shadowRadius: 4.65,
+            elevation: 10,
+          }}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+              SBOOKXXXXXXX
+            </Text>
+            <View
+              style={{
+                backgroundColor: 'blue',
+                borderRadius: 10,
+                paddingHorizontal: 12,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text style={{color: 'white'}}>Confirm</Text>
+            </View>
+          </View>
+          <Text style={{fontSize: 14, color: 'black'}}>16/03/2024</Text>
+          <View
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+              marginVertical: 10, // Sesuaikan dengan margin yang diinginkan
+            }}
+          />
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>
+              09123213213
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: 'black',
+                fontWeight: 'bold',
+                color: 'blue',
+              }}>
+              Warranty
+            </Text>
+          </View>
+          <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+            NBAXZBOOK
+          </Text>
+          <Text style={{fontSize: 14, color: 'black'}}>
+            Mybook 8/SSD256/Core-i3/Blue
+          </Text>
+          <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+            P. Amir - 081399999999
+          </Text>
+          <Text style={{fontSize: 14, color: 'black'}}>LCD Rusak</Text>
+        </TouchableOpacity>
+        {/* <Text>Home Screen</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            /> */}
+      </View>
+    );
+  };
+
+  function HomeScreen({navigation}) {
+    const Icon = () => {
+      if (tab === 'new') {
+        return (
+          <>
+            {header()}
+            {home()}
+          </>
+        );
+      }
+
+      if (tab === 'confirm') {
+        return (
+          <>
+            {header()}
+            {detailScreen()}
+          </>
+        );
+      }
+    };
+
+    return (
+      <View style={{flex: 1}}>
+        <Icon />
+      </View>
+    );
+  }
+
+  function DetailsScreen() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        {header()}
+        {detailScreen()}
+      </View>
+    );
+  }
+  
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Home">
+    //     <Stack.Screen name="Home" component={HomeScreen} />
+    //     <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Detail' }}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
+
+const styles = StyleSheet.create({});
